1a08744b69224f06805e2707733b5702eaf42c39
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-22 07:24:51 -0700

    Added goal notes

--- ChuckJoke/Form1.cs
+++ ChuckJoke/Form1.cs
@@ -12,6 +12,11 @@ namespace ChuckJoke
 {
     public partial class Form1 : Form
     {
+
+        //Once you have the project you have two goals
+        //Add a button to click that will get a random joke using the API included in the solution
+        //Add a dropdown list and display a list of categories in the dropdown when the form first loads, again using the API included in the solution
+
         public Form1()
         {
             InitializeComponent();
