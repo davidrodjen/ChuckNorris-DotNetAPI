64412f2085e1b12e8bb40c58746b48b8e14c7e33
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-24 12:48:21 -0700

    Adjusted Form

--- ChuckJoke/Form1.Designer.cs
+++ ChuckJoke/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.getJokeBtn = new System.Windows.Forms.Button();
-            this.jokeTxt = new System.Windows.Forms.TextBox();
             this.categoryBx = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -43,14 +42,6 @@
             this.getJokeBtn.Text = "Get A Joke!";
             this.getJokeBtn.UseVisualStyleBackColor = true;
             this.getJokeBtn.Click += new System.EventHandler(this.getJokeBtn_ClickAsync);
-            // 
-            // jokeTxt
-            // 
-            this.jokeTxt.Enabled = false;
-            this.jokeTxt.Location = new System.Drawing.Point(12, 119);
-            this.jokeTxt.Name = "jokeTxt";
-            this.jokeTxt.Size = new System.Drawing.Size(228, 20);
-            this.jokeTxt.TabIndex = 1;
             // 
             // categoryBx
             // 
@@ -73,13 +64,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 229);
+            this.ClientSize = new System.Drawing.Size(353, 132);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.categoryBx);
-            this.Controls.Add(this.jokeTxt);
             this.Controls.Add(this.getJokeBtn);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Chuck Norris Jokes";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,7 +78,6 @@
         #endregion
 
         private System.Windows.Forms.Button getJokeBtn;
-        private System.Windows.Forms.TextBox jokeTxt;
         private System.Windows.Forms.ComboBox categoryBx;
         private System.Windows.Forms.Label label1;
     }
